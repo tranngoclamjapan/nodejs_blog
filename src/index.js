c9875b43397d434a8efcce19bfe8539eb8435a4f
TranLam <tranngoclamjapan@gmail.com>
2021-07-19 19:59:48 +0700

    Router & Controller

--- src/index.js
+++ src/index.js
@@ -3,26 +3,30 @@ const express = require('express');
 const morgan = require('morgan');
 const handlebars = require('express-handlebars');
 const app = express();
-const port = 3003;
+const port = 3002;
 
+const route = require('./routes');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(morgan('combined'));
+app.use(express.urlencoded({
+  extended: true
+}));
+app.use(express.json());
+
+///XMLhttpRequest . fetch, axois
 
 app.engine('hbs', handlebars({
   extname: '.hbs'
 }));
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources\\views'));
-// function(req, res) {
-//     return res.send('Hello World');
-// }
-app.get('/', (req, res) => {
-  res.render('home');
-})
 
-app.get('/news', (req, res) => {
-  res.render('news');
-})
+    app.get('/news', (req, res) => {
+        res.render('news');
+      });
+// Route init
+route(app);
+
 //127.0.0.1 - localhost
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
